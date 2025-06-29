9e05148af74c5c65e7a02d7db9b84f4b88255e77
Darkonic10 <tiynov2@mail.ru>
2025-06-29 19:51:08 +0500

    config: change vite config and add global declaration for css modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './',
+  publicDir: 'public',
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
+  build: {
+    minify: 'esbuild',
+    sourcemap: false,
+    target: 'esnext',
+  },
+});
